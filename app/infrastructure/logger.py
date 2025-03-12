b64f21337cbb26cbdd9a2193f1a0b3bba71aa4fb
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-03-12 10:01:47 +0100

    added logging

---
+++ app/infrastructure/logger.py
@@ -0,0 +1,30 @@
+# Logging module to proccess messages in the application
+import logging
+import sys
+from dotenv import load_dotenv
+import os
+
+class Logger():
+    def __init__(self):
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        load_dotenv('../../.env')
+        file_handler=logging.FileHandler(os.getenv('LOG_FILE'))
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+        self.logger.addHandler(file_handler)
+    
+    def info(self, message):
+        self.logger.info(message)
+       
+    def error(self, message):
+        self.logger.error(message)
+    
+    def warning(self, message):
+        self.logger.warning(message)
+    
+    def debug(self, message):
+        self.logger.debug(message)        
